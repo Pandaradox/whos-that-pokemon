8f94eb828e5800a54fd0a75b6793da71c0356c41
John Cardarella <42473778+Pandaradox@users.noreply.github.com>
2023-10-17 00:02:14 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -4,10 +4,35 @@ const pokemonImage = document.querySelector('#pokemon-image');
 const textOverlay = document.querySelector('#text-overlay');
 const choices = document.querySelector('#choices');
 const playBtn = document.querySelector('#play');
-playBtn.addEventListener('click', fetchData);
-loadVoice();
+const points = document.querySelector('#points')
+const time = document.querySelector('#time')
+const qualifierScore = 12; //17 for challenging
+
+let isPlaying = false;
+let score = 0;
+
+playBtn.addEventListener('click', startOver);
 addAnswerHandler();
 
+function startOver() {
+score=0;
+let count = 60;
+isPlaying = true;
+
+points.innerHTML = `<button>Score: ${score}</button>`
+time.innerHTML = `<button>Time: ${count}</button>`
+
+fetchData();
+
+const timer = setInterval(function() {
+  count--;
+  time.innerHTML = `<button>Time: ${count}</button>`
+  if (count === 0) {
+    clearInterval(timer);
+    isPlaying = false;
+  }
+}, 1000);
+}
 
 async function fetchData() {
   resetImage();
@@ -41,16 +66,22 @@ function addAnswerHandler() {
     const { name } = e.target.dataset;
     const resultClass = (name === gameData.correct.name) ?
       'correct' : 'incorrect';
-
+    if (resultClass == 'correct' && isPlaying) {
+      score++;
+      points.innerHTML = `<button>Score: ${score}</button>`
+      if (score >= qualifierScore) {
+        points.classList.add('passed');
+      }
+    }
     e.target.classList.add(resultClass);
     revealPokemon();
-    speakAnswer();
   });
 }
 
 function revealPokemon() {
   main.classList.add('revealed');
   textOverlay.textContent = `${gameData.correct.name}!`;
+  if (isPlaying) fetchData();
 }
 
 function loadVoice() {
